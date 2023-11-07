40641bc262614ed37c5a0216179adca800711232
Svetlana Nasekina <liylic@rambler.ru>
2023-11-07 22:04:08 +0300

    добавили задачу02

---
+++ task02/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти большее из двух чисел.
+
+Console.WriteLine("Введите первое число: ");
+    int firstNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число: ");
+    int secondNumber= Convert.ToInt32(Console.ReadLine());
+    
+       if(firstNumber < secondNumber)
+       { Console.WriteLine($"Первое число {firstNumber} меньше чем второе число {secondNumber}");     }
+else if (secondNumber < firstNumber)
+  { Console.WriteLine($"Первое число {firstNumber} больше чем второе число {secondNumber}");}
+    else if(firstNumber == secondNumber)
+    { Console.WriteLine($"Введенные числа равны {firstNumber}");}
+
+  
