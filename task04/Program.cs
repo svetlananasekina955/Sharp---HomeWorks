cb7ead4478e944a57f2217b37be1a9f40736406d
Svetlana Nasekina <liylic@rambler.ru>
2023-11-07 22:05:43 +0300

    добавили задачу04

---
+++ task04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти максимальное из трех чисел.
+Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int c =  Convert.ToInt32(Console.ReadLine());
+    
+     int result = a;
+       if(b >result)
+        result = b;
+        
+          if(c > result)
+          result = c;
+          
+         Console.WriteLine($"Наибольшее число из трех {result}");
+         
+  
+  
