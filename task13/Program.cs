21897fb78f3e3a0a98499b7f5e2b5a4ec807d68c
Svetlana Nasekina <liylic@rambler.ru>
2023-11-08 14:42:19 +0300

    добавили задачу 13

---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// показать третью цифру заданного числа, или сообщить, что третьей цифры нет
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ 
+ 
+ int thirdDigit = (int)(number / Math.Pow(10, (int)Math.Log10(number) - 2)) % 10;
+
+Console.WriteLine(thirdDigit);
+
+if(number < 100)
+Console.WriteLine("третьей цифры нет");
