82af3d4b8de93076890566b5dbac3111a4e037e5
Svetlana Nasekina <liylic@rambler.ru>
2023-11-08 09:33:06 +0300

    добавили задачу 06

---
+++ task06/Program.cs
@@ -0,0 +1,10 @@
+﻿//проверка на четность
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+         if(number % 2 == 0)
+      {  Console.WriteLine($"Число {number} четное");  }
+      
+          else
+        { Console.WriteLine($"Число {number} нечетное"); 
+         }
+
