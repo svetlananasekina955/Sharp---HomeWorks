e99e922fe66814751b23743d60ed515d145b38cf
Svetlana Nasekina <liylic@rambler.ru>
2023-11-08 09:34:22 +0300

    добавили задачу 08

---
+++ task08/Program.cs
@@ -0,0 +1,7 @@
+﻿// написать программу, которая выводит все четные числа от 1 до number , через знак табуляции
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+    
+      for(int i = 2; i <= number; i+=2)
+      System.Console.Write(i + "    ");
+    
