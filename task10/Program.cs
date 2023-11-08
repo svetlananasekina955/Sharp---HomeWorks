f75ddf5e319bb8090503a0977d2429127540462a
Svetlana Nasekina <liylic@rambler.ru>
2023-11-08 14:00:25 +0300

    добавили задачу 10

---
+++ task10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Ввести трехзначное число, на выходе показать вторую цифру этого числа.
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ int result = number / 10;
+ int result1 = result % 10;
+ Console.WriteLine(result1);
